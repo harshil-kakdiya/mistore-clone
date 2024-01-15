f286283beea2c6ea1403f82c2db8d6f875b4b6b8
harshil-kakdiya <harshilpatel0334@gamil.com>
2024-01-15 16:40:00 +0530

    blank screen problem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import PreNavbar from './components/PreNavbar';
 import Navbar from "./components/Navbar.js"
-import {  BrowserRouter as Router,Route, Routes } from "react-router-dom"
+import {  HashRouter as Router,Route, Routes } from "react-router-dom"
 import Slider from "./components/Slider.js"
 import data from "./data/data.json"
 import Offers from "./components/Offers.js"
